f7a2f4471406f16c0b9862b642b16d9321fa9ca8
jhonmanuelg <jhonmanuelg6@outlook.com>
2021-10-08 14:19:55 -0300

    Subendo actualisação productolista, dataprovider  productoitem e data

--- src/components/productos/ProductoLista.js
+++ src/components/productos/ProductoLista.js
@@ -1,7 +1,14 @@
-import React from "react";
-import IMG from "../images/img01.jpg";
+import React, { useContext } from "react";
+//import IMG from "../images/img01.jpg";
 import Portada from "../images/inicio2.jpg";
+import { DataContext } from "../context/Dataprovider"
+import { ProductoItem } from "./ProductoItem";
+
 export const ProductoLista = () => {
+   const value = useContext(DataContext)
+   const [productos] = value.productos
+   console.log(productos)
+
   return (
     <>
     <div className="inicio2">
@@ -9,7 +16,22 @@ export const ProductoLista = () => {
     </div>
       <h1 className="titel">PRODUCTOS</h1>
       <div className="productos">
-        <div className="producto">
+            {productos.map(productos =>(
+                <ProductoItem 
+                key={productos.id}
+                id={productos.id}
+                title={productos.title}
+                price={productos.price}
+                image={productos.image}
+                Category={productos.Category}                    
+                cantidad={productos.cantidad}                             
+                
+                />
+              ))
+            }
+
+       
+        {/* <div className="producto">
           <a href=" #">
             <div className="producto_img">
               <img src={IMG} alt="sapato negro"  />
@@ -30,9 +52,9 @@ export const ProductoLista = () => {
               </a>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className="producto">
+        {/* <div className="producto">
           <a href=" #">
             <div className="producto_img">
               <img src={IMG} alt="sapato negro" />
@@ -191,30 +213,8 @@ export const ProductoLista = () => {
               </a>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <div className="producto">
-          <a href=" #">
-            <div className="producto_img">
-              <img src={IMG} alt="sapato negro" />
-            </div>
-          </a>
-
-          <div className="producto_footer">
-            <h1> Title </h1>
-            <p> Categoria </p>
-            <p className="price">$500</p>
-          </div>
-
-          <div className="buttom">
-            <button className="button">+ Carrinho</button>
-            <div>
-              <a href="# " className="btn">
-                Vista{" "}
-              </a>
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
